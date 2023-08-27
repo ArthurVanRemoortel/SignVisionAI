01efec549da9db17a7ad776b07a4c0d1b5a3f1e4
ArthurVanRemoortel <arthurvanremoortel@gmail.com>
2023-08-27 20:16:30 +0200

    fix: Cleaned up the interactive demo script.

--- signvision_ai/analyzers/interactive.py
+++ signvision_ai/analyzers/interactive.py
@@ -1,13 +1,9 @@
 import json
 from pathlib import Path
-
 import cv2
 import requests
 from mediapipe.tasks.python.components.containers import Category
 from mediapipe.tasks.python.vision import HandLandmarkerResult
-# from tqdm import tqdm
-# from mediapipe.tasks.python.components.containers.landmark import NormalizedLandmark
-# from signvision_ai.dataset import GestureDatasetEntry, GestureFrame, Coordinate, HandFrame
 import mediapipe as mp
 from mediapipe import solutions
 from mediapipe.framework.formats import landmark_pb2
@@ -30,40 +26,39 @@ HANDEDNESS_TEXT_COLOR = (88, 205, 54)  # vibrant green
 
 
 def draw_landmarks_on_image(rgb_image, detection_result):
-  hand_landmarks_list = detection_result.hand_landmarks
-  handedness_list = detection_result.handedness
-  annotated_image = np.copy(rgb_image)
+    hand_landmarks_list = detection_result.hand_landmarks
+    handedness_list = detection_result.handedness
+    annotated_image = np.copy(rgb_image)
 
-  # Loop through the detected hands to visualize.
-  for idx in range(len(hand_landmarks_list)):
-    hand_landmarks = hand_landmarks_list[idx]
-    handedness = handedness_list[idx]
+    # Loop through the detected hands to visualize.
+    for idx in range(len(hand_landmarks_list)):
+        hand_landmarks = hand_landmarks_list[idx]
+        handedness = handedness_list[idx]
+        # Draw the hand landmarks.
+        hand_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+        hand_landmarks_proto.landmark.extend([
+          landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in hand_landmarks
+        ])
+        solutions.drawing_utils.draw_landmarks(
+          annotated_image,
+          hand_landmarks_proto,
+          solutions.hands.HAND_CONNECTIONS,
+          solutions.drawing_styles.get_default_hand_landmarks_style(),
+          solutions.drawing_styles.get_default_hand_connections_style())
 
-    # Draw the hand landmarks.
-    hand_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
-    hand_landmarks_proto.landmark.extend([
-      landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in hand_landmarks
-    ])
-    solutions.drawing_utils.draw_landmarks(
-      annotated_image,
-      hand_landmarks_proto,
-      solutions.hands.HAND_CONNECTIONS,
-      solutions.drawing_styles.get_default_hand_landmarks_style(),
-      solutions.drawing_styles.get_default_hand_connections_style())
+        # Get the top left corner of the detected hand's bounding box.
+        height, width, _ = annotated_image.shape
+        x_coordinates = [landmark.x for landmark in hand_landmarks]
+        y_coordinates = [landmark.y for landmark in hand_landmarks]
+        text_x = int(min(x_coordinates) * width)
+        text_y = int(min(y_coordinates) * height) - MARGIN
 
-    # Get the top left corner of the detected hand's bounding box.
-    height, width, _ = annotated_image.shape
-    x_coordinates = [landmark.x for landmark in hand_landmarks]
-    y_coordinates = [landmark.y for landmark in hand_landmarks]
-    text_x = int(min(x_coordinates) * width)
-    text_y = int(min(y_coordinates) * height) - MARGIN
+        # Draw handedness (left or right hand) on the image.
+        cv2.putText(annotated_image, f"{handedness[0].category_name}",
+                    (text_x, text_y), cv2.FONT_HERSHEY_DUPLEX,
+                    FONT_SIZE, HANDEDNESS_TEXT_COLOR, FONT_THICKNESS, cv2.LINE_AA)
 
-    # Draw handedness (left or right hand) on the image.
-    cv2.putText(annotated_image, f"{handedness[0].category_name}",
-                (text_x, text_y), cv2.FONT_HERSHEY_DUPLEX,
-                FONT_SIZE, HANDEDNESS_TEXT_COLOR, FONT_THICKNESS, cv2.LINE_AA)
-
-  return annotated_image
+    return annotated_image
 
 
 if __name__ == '__main__':
@@ -105,7 +100,7 @@ if __name__ == '__main__':
             hand_landmarker_result = hands_landmarker.detect_for_video(mp_frame, timestamp_ms)
             face_landmarker_result = face_landmarker.detect_for_video(mp_frame, timestamp_ms)
             frame = draw_landmarks_on_image(frame, hand_landmarker_result)
-            if hand_landmarker_result.hand_landmarks:# and face_landmarker_result.face_landmarks:
+            if hand_landmarker_result.hand_landmarks:
                 time_without_hand = 0
                 gesture_frame = create_gesture_frame(
                     hand_landmarker_result, face_landmarker_result.face_landmarks[0] if face_landmarker_result.face_landmarks else None,
@@ -114,25 +109,24 @@ if __name__ == '__main__':
                 )
                 gesture_data.add_frame(gesture_frame)
 
-                left_sin = process_hand_orientation([gesture_frame.LEFT])[0]
-                right_sin = process_hand_orientation([gesture_frame.RIGHT])[0]
+                left_angle = process_hand_orientation([gesture_frame.LEFT])[0]
+                right_angle = process_hand_orientation([gesture_frame.RIGHT])[0]
                 # print(right_sin)
-                cv2.putText(frame, f"L: {left_sin[0]:.2f}",
+                cv2.putText(frame, f"L: {left_angle[0]:.2f}",
                             (20, 20), cv2.FONT_HERSHEY_DUPLEX,
                             FONT_SIZE, HANDEDNESS_TEXT_COLOR, FONT_THICKNESS, cv2.LINE_AA)
-                cv2.putText(frame, f"R: {right_sin[0]:.2f}",
+                cv2.putText(frame, f"R: {right_angle[0]:.2f}",
                             (20, 40), cv2.FONT_HERSHEY_DUPLEX,
                             FONT_SIZE, HANDEDNESS_TEXT_COLOR, FONT_THICKNESS, cv2.LINE_AA)
 
             else:
                 time_without_hand += (1.0 / fps)
 
-
             if time_without_hand >= 1:
                 if len(gesture_data.frames) > 3:
                     # url = "http://192.168.50.103:8090/api/classify_gesture/"
                     url = "http://signvision.arthurvanremoortel.me/api/classify_gesture/"
-                    #url = "http://localhost:8000/api/classify_gesture/"
+                    # url = "http://localhost:8000/api/classify_gesture/"
                     data = {
                         "language": "VGT",
                         "gesture": gesture_data.to_json(),
